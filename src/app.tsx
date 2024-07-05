8d5020633cc32f9236edbb0c175e56a718ad2f08
Eduardo de Almeida Fernandes <eduardoa.fernandes@hotmail.com>
2024-07-05 13:12:01 -0300

    First commit

---
+++ src/app.tsx
@@ -0,0 +1,14 @@
+export function App() {
+  return (
+    <div className="py-10 space-y-8">
+      <div>
+        Header
+        tabs
+      </div>
+      <main className="max-w-6xl mx-auto space-y-5">
+        <p>oi</p>
+        <p>asd</p>
+      </main>
+    </div>
+  )
+}
